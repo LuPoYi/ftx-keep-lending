3a3f987adc69af143bfc9a2c212ac43d5d110b06
Bob <maydaybob2000@gmail.com>
2020-12-26 21:04:23 +0800

    Update leading amount to total

--- main.js
+++ main.js
@@ -16,17 +16,16 @@ const getFreeBalanceAndLending = async (coin = 'USD') => {
       path: '/wallet/balances',
     })
 
-    const freeBalance = getBalancesResult?.result?.find((item) => item.coin === 'USD')?.free
+    const { free, total } = getBalancesResult?.result?.find((item) => item.coin === 'USD') || {}
+    console.log(new Date(), 'freeBalance', free, 'totalBalance', total)
 
-    console.log(new Date(), 'freeBalance', freeBalance)
-
-    if (freeBalance > 0) {
+    if (total > 0) {
       const offersResult = await ftx.request({
         method: 'POST',
         path: '/spot_margin/offers',
         data: {
           coin: coin,
-          size: freeBalance,
+          size: total,
           rate: 0.000005, // (4.38% / year)
         },
       })
@@ -38,7 +37,8 @@ const getFreeBalanceAndLending = async (coin = 'USD') => {
   }
 }
 
-const job = new CronJob('11 * * * *', function () {
+const job = new CronJob('7 * * * *', function () {
   getFreeBalanceAndLending()
 })
 job.start()
+getFreeBalanceAndLending()
