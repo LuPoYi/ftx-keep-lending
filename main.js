e8aab1f43c6c6c13e9715abbb2f4e4eca6b0858c
Bob <maydaybob2000@gmail.com>
2021-01-03 22:35:08 +0800

    Support multiple coin

--- main.js
+++ main.js
@@ -9,36 +9,44 @@ const ftx = new FTXRest({
   subaccount: process.env.FTX_SUB_ACCOUNT,
 })
 
-const getFreeBalanceAndLending = async (coin = 'USD') => {
+const getFreeBalanceAndLending = async (coins) => {
   try {
     const getBalancesResult = await ftx.request({
       method: 'GET',
       path: '/wallet/balances',
     })
 
-    const { free, total } = getBalancesResult?.result?.find((item) => item.coin === 'USD') || {}
-    console.log(new Date(), 'freeBalance', free, 'totalBalance', total)
+    for (const coin of coins) {
+      const { free, total } = getBalancesResult?.result?.find((item) => item.coin === coin) || {}
+      console.log(new Date(), coins, 'freeBalance', free, 'totalBalance', total)
 
-    if (total > 0) {
-      const offersResult = await ftx.request({
-        method: 'POST',
-        path: '/spot_margin/offers',
-        data: {
-          coin: coin,
-          size: total,
-          rate: 0.000005, // (4.38% / year)
-        },
-      })
+      if (total > 0) {
+        const offersResult = await ftx.request({
+          method: 'POST',
+          path: '/spot_margin/offers',
+          data: {
+            coin: coin,
+            size: parseFloat(total).toFixed(6),
+            rate: 0.000005, // minimun hourly rate => (4.38% / year)
+          },
+        })
 
-      console.log(new Date(), 'offersResult', offersResult)
+        console.log(new Date(), 'offersResult', offersResult)
+      }
     }
   } catch (e) {
     console.log(e.message)
   }
 }
+
+let leading_coins = ['USD']
+if (process.env.LENDING_COIN) {
+  leading_coins = process.env.LENDING_COIN.split(',')
+}
+
 console.log('Before job instantiation')
-const job = new CronJob('7 * * * *', function () {
-  getFreeBalanceAndLending()
+const job = new CronJob('45 * * * *', function () {
+  getFreeBalanceAndLending(leading_coins)
 })
 console.log('After job instantiation')
 job.start()
